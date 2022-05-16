8eb0138bef462d162767309c472baa3825c09e14
Bárbara Salla <barbara.msalla@gmail.com>
2022-05-16 20:39:34 -0300

    incremento e modificação de dependencias, pacote configuration e arquivos properties

--- blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
@@ -15,6 +15,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -26,6 +28,7 @@ public class Usuario {
 	@NotBlank(message = "O campo nome é obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotBlank(message = "O campo usuário é obrigatório!")
 	@Email(message = "Digite um email válido!")
 	private String usuario;
